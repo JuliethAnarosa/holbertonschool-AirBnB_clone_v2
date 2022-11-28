14368290a2e573d4c2840c0ffd0d3a13f1da0126
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-28 02:53:55 +0000

    update table user

--- models/user.py
+++ models/user.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+""" holds class User"""
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+storecondition = getenv("HBNB_TYPE_STORAGE")
+
+
+class User(BaseModel, Base):
+    """Representation of a user """
+    if storecondition == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user", cascade="all, delete",
+                               passive_deletes=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
