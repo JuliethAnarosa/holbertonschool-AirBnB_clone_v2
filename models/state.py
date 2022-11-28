529543184667ca6cfe27466956921ae01926628c
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-28 01:33:41 +0000

    update

--- models/state.py
+++ models/state.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
+import models
+
+storecondition = getenv("HBNB_TYPE_STORAGE")
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if storecondition == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="states", cascade="all, delete",
+                              passive_deletes=True)
+    else:
+        name = ""
+
+    if storecondition != "db":
+        @property
+        def cities(self):
+            """Returns a list of City instances with state_id equals
+            to the current State.id"""
+            citylist = []
+            allcitys = models.storage.all(City).values()
+            for city in allcitys:
+                if city.state_id == self.id:
+                    citylist.append(city)
+            return citylist
