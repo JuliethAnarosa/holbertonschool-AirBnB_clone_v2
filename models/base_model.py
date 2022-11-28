8e02c5d712d2612842a8d2bd3ca52a261529fd8a
UNICA-FIME <juancarlosfime@gmail.com>
2022-11-28 03:03:55 +0000

    correction

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+import sqlalchemy
 import uuid
+from os import getenv
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-import models
-import os
+import time
 
 
-if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+storecondition = getenv("HBNB_TYPE_STORAGE")
+
+
+if storecondition == "db":
     Base = declarative_base()
 else:
     Base = object
@@ -16,38 +20,39 @@ else:
 
 class BaseModel:
     """A base class for all hbnb models"""
-    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
-        id = Column(String(60), primary_key=True, nullable=False)
-        created_at = Column(DateTime, default=datetime.utcnow())
-        updated_at = Column(DateTime, default=datetime.utcnow())
+    if storecondition == "db":
+        id = Column(String(60), nullable=False, primary_key=True)
+        created_at = Column(
+            DateTime, nullable=False, default=datetime.utcnow())
+        updated_at = Column(
+            DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
+            """storage.new(self)"""
         else:
-            if 'id' not in kwargs:
-                kwargs['id'] = str(uuid.uuid4())
-            if 'updated_at' not in kwargs:
-                kwargs['updated_at'] = datetime.now()
-            else:
+            if 'updated_at' in kwargs.keys():
                 kwargs['updated_at'] = datetime.strptime(
-                                       kwargs['updated_at'],
-                                       '%Y-%m-%dT%H:%M:%S.%f')
-            if 'created_at' not in kwargs:
-                kwargs['created_at'] = datetime.now()
-            else:
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if 'created_at' in kwargs.keys():
                 kwargs['created_at'] = datetime.strptime(
-                                       kwargs['created_at'],
-                                       '%Y-%m-%dT%H:%M:%S.%f')
-            if '__class__' in kwargs:
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if '__class__' in kwargs.keys():
                 del kwargs['__class__']
+            if not kwargs.get('id'):
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
             self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
+        if "_sa_instance_state" in self.__dict__:
+            del self.__dict__["_sa_instance_state"]
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
@@ -55,8 +60,8 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
+        storage.new(self)
+        storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -71,5 +76,10 @@ class BaseModel:
         return dictionary
 
     def delete(self):
-        """delete the current instance from the storage"""
-        models.storage.delete(self)
+        """
+        ---------------------------------------------
+        delete the current instance from the storage
+        (models.storage) by calling the method delete
+        ---------------------------------------------
+        """
+        pass
