7120d532a694e13582e235c429a66a9c5c48027d
JuliethAnarosa <america02anarosa@gmail.com>
2022-11-30 14:40:07 -0500

    fix user

--- models/user.py
+++ models/user.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-""" holds class User"""
+"""This module defines a class User"""
+import models
 from models.base_model import BaseModel, Base
 import sqlalchemy
-from os import getenv
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 
-storecondition = getenv("HBNB_TYPE_STORAGE")
-
-
 class User(BaseModel, Base):
-    """Representation of a user """
-    if storecondition == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user", cascade="all, delete",
-                               passive_deletes=True)
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    """This class defines a user by various attributes"""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship('Place', backref='user', cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
+
+    def __init__(self, *args, **kwargs):
+        """
+        inherit from base  and Basemodel init
+        """
+        super().__init__(*args, **kwargs)
