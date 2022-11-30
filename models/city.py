89c047162867801ea1d505746f8da7deb4b78c4d
JuliethAnarosa <america02anarosa@gmail.com>
2022-11-30 00:37:14 -0500

    city, base model

--- models/city.py
+++ models/city.py
@@ -11,10 +11,13 @@ storecondition = getenv("HBNB_TYPE_STORAGE")
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    if storecondition == "db":
-        __tablename__ = "cities"
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        name = Column(String(128), nullable=False)
-    else:
-        state_id = ""
-        name = ""
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="cities")
+
+    def __init__(self, *args, **kwargs):
+        """
+            Init for inherited
+        """
+        super().__init__(*args, **kwargs)
